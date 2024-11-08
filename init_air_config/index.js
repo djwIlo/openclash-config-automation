d5db0a4f232714d2b7b740a726786b65fb8d6876
duanjiwang <djwilo@126.com>
2024-11-08 14:48:04 +0800

    配置文件初始化生成完成

--- init_air_config/index.js
+++ init_air_config/index.js
@@ -7,16 +7,12 @@ const initAirConfig = () => {
 
   // 机场配置序列化数组
   const airConfigArray = utils.readAirConfig(AIRPORT_CONFIG.configFilePath);
-
+  
   // 获取指定代理、代理组、规则行所索引
   let arrayIndex = utils.getRegExpIndex(airConfigArray);
   
   // 生成自定义代理配置
-  const customProxyConfig = utils.getCustomProxyConfig(AIRPORT_CONFIG.listProxyseller, AIRPORT_CONFIG);
-  
-  // 生成自定义链式代理组配置
-  // >>>
-  // const customProxyGroppConfig = utils.getCustomProxyGroppConfig(AIRPORT_CONFIG.listRuleseller);
+  const { customProxyConfig, proxySerialize } = utils.getCustomProxyConfig(AIRPORT_CONFIG.listProxyseller, AIRPORT_CONFIG);  
 
   // 生成自定义规则配置
   const { ruleConfig, storageProxy } = utils.getCustomRuleConfig(AIRPORT_CONFIG.listRuleseller, AIRPORT_CONFIG.listProxyseller);
@@ -28,22 +24,25 @@ const initAirConfig = () => {
     console.log('lanIp历史使用记录写入失败', error);
   }
 
-  // 开始组合配置 
+  // 开始组合配置
   // 插入rule
   airConfigArray.splice(arrayIndex.ruleStartIndex, 0, ...ruleConfig);
 
+  // 获取机场代理配置、香港代理组、链式代理组
+  const { getAirProxyConfig, mergeProxyGroup } = utils.getConfigProxySlice(arrayIndex, airConfigArray, proxySerialize)
+
   // 插入链式代理组
-  // >>>
+  airConfigArray.splice(arrayIndex.proxyEndIndex + 2, 0, ...mergeProxyGroup);
 
   // 插入代理
-  const getAirProxyConfig = utils.getConfigProxySlice(arrayIndex, airConfigArray)
-
   const mergeProxyConfig = customProxyConfig.concat(...getAirProxyConfig);
 
   arrayIndex = utils.getRegExpIndex(airConfigArray);
-  
-  airConfigArray.splice(arrayIndex.proxyStartIndex, arrayIndex.proxyEndIndex - arrayIndex.proxyStartIndex + 1, ...mergeProxyConfig);
 
+  airConfigArray.splice(arrayIndex.proxyStartIndex, arrayIndex.proxyEndIndex - arrayIndex.proxyStartIndex + 1, mergeProxyConfig);
+
+  arrayIndex = utils.getRegExpIndex(airConfigArray);
+  
   fs.writeFileSync(AIRPORT_CONFIG.outputConfigPath, airConfigArray.join(''));
 }
 
--- init_air_config/utils.js
+++ init_air_config/utils.js
@@ -1,17 +1,17 @@
-const fs = require('fs');
-const getArpScanResults = require('./lanIp');
-const getProxyResult = require('./proxyIp');
+const fs = require("fs");
+const getArpScanResults = require("./lanIp");
+const getProxyResult = require("./proxyIp");
 
 /**
  * 机场配置序列化
- * @param {string} file 
+ * @param {string} file
  * @returns {Array}
  */
 function readAirConfig(file) {
   try {
-    const airText = fs.readFileSync(file, 'utf8');
+    const airText = fs.readFileSync(file, "utf8");
 
-    const airSerialize = airText.split('\n').map(item => item + '\n');
+    const airSerialize = airText.split("\n").map((item) => item + "\n");
 
     return airSerialize;
   } catch (error) {
@@ -25,7 +25,7 @@ function readAirConfig(file) {
  * @returns {Object} arrayIndex
  */
 function getRegExpIndex(configArray) {
-  let arrayIndex = {}
+  let arrayIndex = {};
 
   // 代理开始行正则
   const proxiesStartRegExp = /^proxies:.*/;
@@ -38,15 +38,15 @@ function getRegExpIndex(configArray) {
 
   for (let index = 0; index < configArray.length; index++) {
     const element = configArray[index];
-    if (element.includes('proxies:') && proxiesStartRegExp.test(element)) {
+    if (element.includes("proxies:") && proxiesStartRegExp.test(element)) {
       arrayIndex.proxyStartIndex = index + 1;
     }
 
-    if (element.includes('proxy-groups:') && proxiesEndRegExp.test(element)) {
+    if (element.includes("proxy-groups:") && proxiesEndRegExp.test(element)) {
       arrayIndex.proxyEndIndex = index - 1;
     }
 
-    if (element.includes('rules:') && ruleStartRegExp.test(element)) {
+    if (element.includes("rules:") && ruleStartRegExp.test(element)) {
       arrayIndex.ruleStartIndex = index + 1;
     }
   }
@@ -62,29 +62,42 @@ function getRegExpIndex(configArray) {
  */
 function getCustomProxyConfig(file, AIRPORT_CONFIG) {
   try {
-    let proxyConfig = [];
+    let proxyConfig1 = [];
+    let proxySerialize = [];
     const proxyText = fs.readFileSync(file, "utf8");
-    const proxyArray = proxyText.split('\n');
-    proxyArray.forEach(element => {
-      const itemArr = element.split('@');
-      const proxyName = itemArr[0].split(':');
-      const proxyAuth = itemArr[1].split(':');
-      const proxyNetwork = itemArr[2].split(':');
-      const proxy = new AIRPORT_CONFIG(proxyName[0], proxyNetwork[0], proxyNetwork[1], proxyAuth[0], proxyAuth[1]);
-      proxyConfig.push(proxy.proxyConfig.join('\n') + '\n');
+    const proxyArray = proxyText.split("\n");
+    proxyArray.forEach((element) => {
+      const itemArr = element.split("@");
+      const proxyName = itemArr[0].split(":");
+      const proxyAuth = itemArr[1].split(":");
+      const proxyNetwork = itemArr[2].split(":");
+      const proxy = new AIRPORT_CONFIG(
+        proxyName[0],
+        proxyNetwork[0],
+        proxyNetwork[1],
+        proxyAuth[0],
+        proxyAuth[1]
+      );
+
+      proxyConfig1.push(proxy.proxyConfig.join("\n") + "\n");
+
+      const proxyObj = {
+        proxyState: proxyName[0], // 代理名称
+        linkProxy: proxyName[1], // 链式代理名称
+        auth: proxyAuth[0], // 用户名
+        password: proxyAuth[1], // 密码
+        host: proxyNetwork[0], // IP地址
+        port: parseInt(proxyNetwork[1]) // 端口
+      }
+      proxySerialize.push(proxyObj)
     });
-    
-    return proxyConfig.join('');
+
+    const customProxyConfig = proxyConfig1.join("")
+
+    return { customProxyConfig, proxySerialize };
   } catch (err) {
     console.error("读取文件错误:", err);
   }
-}
-
-/**
- * 生成自定义链式代理组配置
- */
-function getCustomProxyGroppConfig() {
-
 }
 
 /**
@@ -95,19 +108,21 @@ function getCustomProxyGroppConfig() {
  */
 function getCustomRuleConfig(ruleFile, proxyFile) {
   const ruleText = fs.readFileSync(ruleFile, "utf8");
-  const ruleArray = ruleText.split('\n'); //基础rule配置
+  const ruleArray = ruleText.split("\n"); //基础rule配置
   const lanIp = getArpScanResults();
   const proxyIp = getProxyResult(proxyFile);
   const { assignProxy, storageProxy } = assignProxies(lanIp, proxyIp);
-  const ruleConfig = ruleArray.concat(...assignProxy).map(item => item + '\n');
-  return {ruleConfig, storageProxy}
+  const ruleConfig = ruleArray
+    .concat(...assignProxy)
+    .map((item) => item + "\n");
+  return { ruleConfig, storageProxy };
 }
 
 /**
  * 分配代理并存储历史代理使用记录
- * @param {*} lanIp 
- * @param {*} proxyIp 
- * @returns 
+ * @param {*} lanIp
+ * @param {*} proxyIp
+ * @returns
  */
 function assignProxies(lanIp, proxyIp) {
   const assignProxy = lanIp.map((element, index) => {
@@ -129,33 +144,103 @@ function assignProxies(lanIp, proxyIp) {
  * 获取配置文件中的代理片段
  * @param {Object} arrayIndex 索引存储
  * @param {Array} configList 配置文件序列化数组
+ * @param {Array} proxySerialize 自定义代理序列化数组
  * @returns {Array} Array
  */
-function getConfigProxySlice(arrayIndex, configList) {
-  const proxiesList = configList.slice(arrayIndex.proxyStartIndex, arrayIndex.proxyEndIndex);
+function getConfigProxySlice(arrayIndex, configList, proxySerialize) {
+  const proxiesList = configList.slice(
+    arrayIndex.proxyStartIndex,
+    arrayIndex.proxyEndIndex
+  );
   // 配置文件代理配置段
-  let proxyContext = '';
+  let proxyContext = "";
   for (let index = 0; index < proxiesList.length; index++) {
     const element = proxiesList[index];
-    // proxyContext += element + '\n';
     proxyContext += element;
   }
   // 按照 - name 行分割
-  const entries = proxyContext.split(/(?=-\sname:)/).filter(entry => entry.trim() !== '');
+  const entries = proxyContext
+    .split(/(?=-\sname:)/)
+    .filter((entry) => entry.trim() !== "");
   // 每段作为一个数组元素
-  const proxyConfigMap = entries.map(entry => [entry.trim() + '\n']);
-  
+  const proxyConfigMap = entries.map((entry) => [entry.trim() + "\n"]);
+
   // 过滤出机场配置
-  const proxyConfigList = proxyConfigMap.filter(entry => !entry[0].includes('socks5')).join('');
-    
-  return [proxyConfigList];
+  // const proxyConfigList = proxyConfigMap.filter(entry => !entry[0].includes('socks5')).join('');
+  const proxyConfigList = proxyConfigMap.filter(
+    (entry) => !entry[0].includes("socks5")
+  );
+
+  // 生成香港代理组
+  const hkProxyGroup = getHKProxyGroup(proxyConfigList);
+
+  // 生成链式代理组
+  const linkProxyGroup = getLinkProxyGroup(proxySerialize)
+  
+  // 合并代理组
+  const mergeProxyGroup = linkProxyGroup.concat(hkProxyGroup)
+
+  const getAirProxyConfig = [proxyConfigList.join("")];
+
+  return { getAirProxyConfig, mergeProxyGroup};
+}
+
+/**
+ * 生成香港代理组
+ * @param {Array} proxyConfigList 机场代理配置片段
+ * @returns 
+ */
+function getHKProxyGroup(proxyConfigList) {
+  let hkProxy = [];
+  for (let index = 0; index < proxyConfigList.length; index++) {
+    const item = proxyConfigList[index];
+    if (item[0].includes("香港")) {
+      const proxyName = item[0].split("- name: ")[1].split("  type:")[0];
+      hkProxy.push(`  - ${proxyName}`);
+    }
+  }
+
+  const hkProxyGroup = [
+    `- name: HK 香港节点\n`,
+    `  type: url-test\n`,
+    `  proxies:\n`,
+    `  url: http://www.gstatic.com/generate_204\n`,
+    `  interval: '300'\n`,
+    `  tolerance: '50'\n`,
+  ];
+
+  hkProxyGroup.splice(3, 0, ...hkProxy);
+
+  return hkProxyGroup;
+}
+
+/**
+ * 生成链式代理组
+ * @param {Array} proxySerialize 自定义代理序列化数组
+ */
+function getLinkProxyGroup(proxySerialize) {
+  let linkProxyGroup = []
+  for (let index = 0; index < proxySerialize.length; index++) {
+    const item = proxySerialize[index];
+    const linkProxy = [
+      `- name: ${item.linkProxy}\n`,
+      `  type: relay\n`,
+      `  proxies:\n`,
+      `    - "HK 香港节点"\n`,
+      `    - "${item.proxyState}"\n`
+    ]
+
+    linkProxyGroup = linkProxyGroup.concat(linkProxy);
+
+  }
+
+  return linkProxyGroup;
 }
 
 module.exports = {
   readAirConfig,
   getRegExpIndex,
   getCustomProxyConfig,
-  getCustomProxyGroppConfig,
   getCustomRuleConfig,
-  getConfigProxySlice
-}
+  getConfigProxySlice,
+};
